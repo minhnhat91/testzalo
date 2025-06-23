0f1d73af274ca7bc981ad5c348f3b158d138f5d0
MrDrunk <minhnhat91@gmail.com>
2025-06-23 01:04:36 +0000

    add zca-js source code

---
+++ zca-js/zca-js/dist/apis/getAliasList.js
@@ -0,0 +1,24 @@
+import { ZaloApiError } from "../Errors/ZaloApiError.js";
+import { apiFactory } from "../utils.js";
+export const getAliasListFactory = apiFactory()((api, ctx, utils) => {
+    const serviceURL = utils.makeURL(`${api.zpwServiceMap.alias[0]}/api/alias/list`);
+    /**
+     * Get alias list
+     *
+     * @throws ZaloApiError
+     */
+    return async function getAliasList() {
+        const params = {
+            page: 1,
+            count: 100,
+            imei: ctx.imei,
+        };
+        const encryptedParams = utils.encodeAES(JSON.stringify(params));
+        if (!encryptedParams)
+            throw new ZaloApiError("Failed to encrypt params");
+        const response = await utils.request(utils.makeURL(serviceURL, { params: encryptedParams }), {
+            method: "GET",
+        });
+        return utils.resolve(response);
+    };
+});
